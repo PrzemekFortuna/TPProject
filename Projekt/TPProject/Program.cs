ace590f1518042e20de026beed36f6a09c3a07a2
Krzysztof Barden <wiedzmenpl@gmail.com>
2018-10-03 22:34:04 -0700

    1st try

---
+++ Projekt/TPProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPProject
+{
+    public static class Program
+    {
+
+        [STAThread]
+        public static int Main(string[] args)
+        {
+                Console.WriteLine("Hello, press enter to enter GUI mode");
+                Console.ReadLine();
+                App.Main();
+                return 0;
+        }
+    }
+}
