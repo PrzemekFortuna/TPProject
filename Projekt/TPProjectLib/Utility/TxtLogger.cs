9698f538bec78d5705273f8daa5e75a5d553a4a9
Przemek Fortuna <przemyslaw.fortuna@gmail.com>
2018-10-13 21:40:16 +0200

    simple file logger

---
+++ Projekt/TPProjectLib/Utility/TxtLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPProjectLib.Utility
+{
+    public class TxtLogger : ILogger
+    {
+        private string _fileName;
+
+        public TxtLogger()
+        {
+            _fileName = "log.txt";
+
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+        }
+
+        public void Log(LogMode mode ,string message)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("[{0}]\t", mode.ToString());
+            sb.AppendFormat("{0}{1}", message, Environment.NewLine);
+
+            File.AppendAllText(_fileName, sb.ToString());   
+        }
+    }
+}
