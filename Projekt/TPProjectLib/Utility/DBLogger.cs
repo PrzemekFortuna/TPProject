e472160439bd1600d762a0aaa22657cb969cdc7f
Przemek Fortuna <przemyslaw.fortuna@gmail.com>
2018-10-17 22:22:55 +0200

    added DBLogger

---
+++ Projekt/TPProjectLib/Utility/DBLogger.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPProjectLib.Utility
+{
+    public class DBLogger : ILogger
+    {
+        private string connectionString = "User ID=npfmrblycmsenb;Password=84e2beaa02230d0c8263938cbcf103aca1d67ea5f3654ce3080f0f277013798a;Host=ec2-54-217-205-90.eu-west-1.compute.amazonaws.com;Port=5432;Database=d9b9j7nr1qnk76;Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True;";
+
+        public void Log(LogMode mode, string message)
+        {
+            Task.Run(() =>
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    using (NpgsqlCommand command = connection.CreateCommand())
+                    {
+                        connection.Open();
+                        command.CommandText = string.Format("INSERT INTO LOGS VALUES ({0}, '{1}')", (int)mode, message);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            });
+        }
+    }
+}
